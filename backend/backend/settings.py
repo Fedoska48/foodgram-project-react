732b3e39c12c88ed84f1f153fe86f4305eafb1b6
Никита Федяев <itsfedos@gmail.com>
2023-02-02 22:22:21 +0300

    for 1 review: server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'foodgram'),
         'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', '1234'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', '5432')
     }
 }
