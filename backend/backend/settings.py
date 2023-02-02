102279c593e0d9f8f554083e4cb81e41335e04c1
Никита Федяев <itsfedos@gmail.com>
2023-02-02 20:38:45 +0300

    for 1 review: server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -143,6 +143,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = ('static',)
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
