3515594640d0e3a359763ab25aaad5ef2cef3c5d
Никита Федяев <itsfedos@gmail.com>
2023-02-03 10:32:51 +0300

    for 1 review: server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='foodgram'),
+        'NAME': os.getenv('POSTGRES_DB', default='foodgram'),
         'USER': os.getenv('POSTGRES_USER', default='foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='1234'),
         'HOST': os.getenv('DB_HOST', default='db'),
