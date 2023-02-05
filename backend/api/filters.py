8988089ddcca3166e706b101ad3062c5d4704a23
Никита Федяев <itsfedos@gmail.com>
2023-02-05 23:11:20 +0300

    for 3 review: app

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -27,20 +27,19 @@ class RecipeFilter(django_filters.FilterSet):
 
     def get_is_favorited(self, queryset, name, value):
         user = self.request.user
-        if value == 1:
+        if value:
             return queryset.filter(
-                favorite_set__user=user,
-                is_favorited=True,
+                favorite__user=user,
             )
         return Recipe.objects.all()
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
-        if value and self.request.user.is_authenticated:
-            return queryset.shoppingcart_set.filter(
-                user=user
+        if value:
+            return queryset.filter(
+                shopping_cart__user=user,
             )
-        return queryset
+        return Recipe.objects.all()
 
 
 class IngredientFilter(SearchFilter):
