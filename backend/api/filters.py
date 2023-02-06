e4610ce03d1a8d2a59c4ab44cdaa9db98c4012d4
Никита Федяев <itsfedos@gmail.com>
2023-02-07 00:33:54 +0300

    for 4 review: app

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,5 @@
 from django_filters import ModelMultipleChoiceFilter
-from django_filters.rest_framework import FilterSet, filters, BooleanFilter
+from django_filters.rest_framework import FilterSet, BooleanFilter
 from rest_framework.filters import SearchFilter
 
 from recipes.models import Ingredient, Recipe, Tag
